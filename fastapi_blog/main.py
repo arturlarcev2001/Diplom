8975d97136173ccbc908af386f2793ef7857a33b
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-11-08 12:02:34 +0300

    Add files via upload

---
+++ fastapi_blog/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+HR = templates.TemplateResponse
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+	return HR("index.html", context={"request": request})
+
+@app.get("/about/", response_class=HTMLResponse)
+def about(request: Request):
+	return HR("about.html", context={"request": request})
+
+@app.get("/user/", response_class=HTMLResponse)
+def user(request: Request, user="default"):
+	return HR("user.html", context={"request": request, "user":user})
+
+@app.get("/post/", response_class=HTMLResponse)
+def post(request: Request):
+	return HR("post.html", context={"request": request})
+
