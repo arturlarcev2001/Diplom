f86ab3ec8db3b2eb26d9c20eebe96a2eea4605e5
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-11-13 20:35:16 +0500

    Add files via upload

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -9,38 +9,62 @@ db.init_app(app)
 
 @app.route('/')
 def index():
+    """
+        Главная страница приложения
+        выводит все новейшие посты первыми
+    """
     posts = Post.newest_first().all()
     return render_template('index.html', posts=posts)
 
+
 @app.route('/post/<int:post_id>')
 def post(post_id):
-    post = Post.query.get_or_404(post_id)
-    return render_template('post.html', post=post)
+    """
+        Этот маршрут, позволяет открыть
+        отдельный пост
+    """
+    current_post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=current_post)
+
 
 @app.route('/category/<int:category_id>')
 def category(category_id):
-    category = Category.query.get_or_404(category_id)
+    """
+        Этот маршрут, позволяет просматривать посты
+        определенной категории
+    """
+    category_obj = Category.query.get_or_404(category_id)
     posts = Post.query.filter_by(category_id=category_id).order_by(Post.date.desc()).all()
-    return render_template('category.html', category=category, posts=posts)
+    return render_template('category.html', category=category_obj, posts=posts)
+
 
 @app.route('/tag/<int:tag_id>')
 def tag(tag_id):
-    tag = Tag.query.get_or_404(tag_id)
+    """
+        Этот маршрут, позволяет просматривать посты
+        определенного тега
+    """
+    tag_obj = Tag.query.get_or_404(tag_id)
     posts = Post.query.filter(Post.tags.any(id=tag_id)).order_by(Post.date.desc()).all()
-    return render_template('tag.html', tag=tag, posts=posts)
+    return render_template('tag.html', tag=tag_obj, posts=posts)
+
 
 @app.route('/new_post', methods=['GET', 'POST'])
 def new_post():
+    """
+        Этот маршрут, дает возможность создать новый
+        пост использую запрост POST
+    """
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
         category_id = request.form['category']
         tag_ids = [int(tag_id) for tag_id in request.form.getlist('tags')]
-        post = Post(title=title, content=content, category_id=category_id)
+        create_post = Post(title=title, content=content, category_id=category_id)
         for tag_id in tag_ids:
-            tag = Tag.query.get(tag_id)
-            post.tags.append(tag)
-        db.session.add(post)
+            post_tag = Tag.query.get(tag_id)
+            create_post.tags.append(post_tag)
+        db.session.add(create_post)
         db.session.commit()
         flash('Запись создана!', 'success')
         return redirect(url_for('index'))
@@ -48,32 +72,35 @@ def new_post():
     tags = Tag.query.all()
     return render_template('new_post.html', categories=categories, tags=tags)
 
+
 @app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
 def edit_post(post_id):
-    post = Post.query.get_or_404(post_id)
+    edit_post = Post.query.get_or_404(post_id)
     if request.method == 'POST':
-        post.title = request.form['title']
-        post.content = request.form['content']
-        post.category_id = request.form['category']
+        edit_post.title = request.form['title']
+        edit_post.content = request.form['content']
+        edit_post.category_id = request.form['category']
         tag_ids = [int(tag_id) for tag_id in request.form.getlist('tags')]
-        post.tags = []
+        edit_post.tags = []
         for tag_id in tag_ids:
             tag = Tag.query.get(tag_id)
-            post.tags.append(tag)
+            edit_post.tags.append(tag)
         db.session.commit()
         flash('Запись обновлена!', 'success')
         return redirect(url_for('index'))
     categories = Category.query.all()
     tags = Tag.query.all()
-    return render_template('edit_post.html', post=post, categories=categories, tags=tags)
+    return render_template('edit_post.html', post=edit_post, categories=categories, tags=tags)
+
 
 @app.route('/delete_post/<int:post_id>', methods=['GET', 'POST'])
 def delete_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    db.session.delete(post)
+    select_post = Post.query.get_or_404(post_id)
+    db.session.delete(select_post)
     db.session.commit()
     flash('Запись удалена!', 'success')
     return redirect(url_for('index'))  
-          
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- flask_blog/models.py
+++ flask_blog/models.py
@@ -1,8 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 db = SQLAlchemy()
 
+
 class Category(db.Model):
+    """
+    Category - категория поста
+    id - уникальный ключ
+    name - имя категории
+    posts - свзяь с постами
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     posts = db.relationship('Post', backref='category', lazy=True)
@@ -10,7 +18,14 @@ class Category(db.Model):
     def __repr__(self):
         return f'<Category {self.name}>'
 
+
 class Tag(db.Model):
+    """
+    Tag - тэг поста
+    id - уникальный ключ
+    name - имя категории
+    posts - свзяь с постами
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     posts = db.relationship('Post', secondary='post_tag', backref='tags', lazy=True)
@@ -18,12 +33,22 @@ class Tag(db.Model):
     def __repr__(self):
         return f'<Tag {self.name}>'
 
+# Создание таблицы с помощью средств SQLAlchemy
 post_tag = db.Table('post_tag',
-    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
-)
+                    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True),
+                    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+                    )
+
 
 class Post(db.Model):
+    """
+        Основная модель в таблице
+        id - Уникальный ключ
+        title - заголовок
+        content - текст поста
+        date - дата публикации
+        category_id - номер категории поста
+    """
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
@@ -32,7 +57,7 @@ class Post(db.Model):
 
     def repr(self):
         return f'<Post {self.title}>'
-    
+
     @classmethod
     def newest_first(cls):
         return cls.query.order_by(cls.date.desc())
